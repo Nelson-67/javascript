d9ea3462f751eb22e75e80131239ab00a39ec7ae
marina-arruda0 <tinelson1727@gmail.com>
2026-09-08 15:34:53 -0300

    add tipo de dados object

---
+++ tipos-de-dados-object.js
@@ -0,0 +1,8 @@
+let pessoas = {
+    nome: "Nelson",
+    idade: 15,
+    aprovado: true
+};
+// Acessando propriedades do objeto
+console.log(pessoas.nome); // "Nelson"
+console.log(pessoas.idade); // 15
