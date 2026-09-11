9e9a8f90335855463f133c74c9871630759ef0ca
Nelson-67 <tinelson1727@gmail.com>
2026-09-11 17:25:57 -0300

    add operador atribuicao modulo

---
+++ operador-atribuicao-modulo.js
@@ -0,0 +1,7 @@
+let totalDoces = 10;
+let criancas = 3;
+
+// Queremos saber quantos doces cada criança vai receber e quantos doces vão sobrar.
+totalDoces %= criancas;
+
+console.log(totalDoces); // Saída: 1
